8fb4e9f3133f4723683ebab2cc982ecaf1ac6b37
Nooh Alavi <noohalavi@gmail.com>
2021-04-28 19:10:12 -0400

    a

---
+++ my-site/src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	export let num = 0;
+	export let text = "Pressed 0 times";
+
+	function increaseNum() {
+		num++;
+		text = `Pressed ${num} times`
+	}
+</script>
+
+<svelte:head>
+	<title>Nooh Alavi</title>
+</svelte:head>
+
+<main>
+	<h1>Nooh Alavi</h1>
+</main>
+
+<style>
+
+</style>
