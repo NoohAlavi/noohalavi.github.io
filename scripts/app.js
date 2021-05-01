51324ee25dec1c2d1f677b37b769821554a39b5d
Nooh Alavi <noohalavi@gmail.com>
2021-05-01 15:46:34 -0400

    Added close warning button

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,9 @@
 function openDiscord() {
     open("https://discord.gg/vxvKSvd")
 }
+
+function closeWarning() {
+    var warning = document.getElementsByClassName("warning")[0];
+    warning.style.display = "none";
+    console.log('a')
+}
