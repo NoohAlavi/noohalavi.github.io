1f919d93b337d5c679214c7997932f400caea8c9
Nooh Alavi <noohalavi@pop-os.localdomain>
2021-08-23 23:17:10 -0400

    Removed about me page, updated home page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,10 +4,9 @@ import styles from "../styles/components/Navbar.module.scss";
 export default function Navbar() {
     return (
         <nav className={styles.navbar}>
-            <h2 className={styles.pageheader}><Link href="/">nooh alavi - developer</Link></h2>
+            {/* <h2 className={styles.pageheader}><Link href="/">nooh alavi - developer</Link></h2> */}
             <ul>
                <li><Link href="/">Home</Link></li>
-               <li><Link href="/about-me">About Me</Link></li>
                <li><Link href="/my-blog">My Blog</Link></li>
             </ul> 
         </nav>
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,10 @@ export default function Home() {
   return (
     <div>
       <Navbar/>
-      <h1 className="page-title">Home</h1>
+      <div className="content home-header">
+        <h1 className="page-title">Nooh Alavi</h1>
+        <h3 className="page-subtitle">High schooler who loves programming and game development!</h3>
+      </div>
       <Footer/>
     </div>
   );
