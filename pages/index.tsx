c6e57b44a4cd83a05947701f852fa35d74ec432b
Nooh Alavi <noohalavi@pop-os.localdomain>
2021-08-25 17:32:32 -0400

    Modified bg gradient colours, added new software section and updates to the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,15 +15,17 @@ export default function Home() {
         <h3 className="page-subtitle">Who Am I?</h3>
         <p className="content">
           My name's Nooh Alavi, I'm fifteen and I love programming! I taught myself how to code when I was ten, through books, YouTube videos, and of course, tons of Googling (thanks StackOverflow!).<br/><br/>
-          Now, as a hobby, I create video games, applications, and websites (like this one!). Check out some of my projects down below, and feel free to <a href="https://github.com/NoohAlavi" target="_blank">contribute</a> to them!
+          Now, as a hobby, I create video games, applications, and websites (like this one!) using <a href="/#software">a variety of languages and tools</a>. Check out some of my projects down below, and feel free to <a href="https://github.com/NoohAlavi" target="_blank">contribute</a> to them!
         </p>
       </div>
       <div className="container">
         <h3 className="page-subtitle" id="myProjects">My Projects</h3>     
       </div>
       <div className="container">
+        <h3 className="page-subtitle" id="software">Software I Work With</h3>
+      </div>
+      <div className="container">
         <h3 className="page-subtitle" id="mySocials">My Socials</h3>
-
       </div>
       <Footer/>
     </div>
