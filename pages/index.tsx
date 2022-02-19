3bbc9c7ac632071ba4e4ed374f9bd89fe3848723
Nooh Alavi <noohalavi@gmail.com>
2022-02-19 18:53:49 -0500

    Made site more reactive

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,17 +9,20 @@ export default function Home() {
       <Navbar/>
       <div className="container">
         <h1 className="page-title">Nooh Alavi</h1>
-        <h3 className="page-subtitle">High schooler who loves programming and game development!</h3>
+        <h3 className="page-subtitle">High schooler who loves technology!</h3>
       </div>
       <div className="container">
         <h3 className="page-subtitle" id="aboutMe">Who Am I?</h3>
         <p className="content">
-          My name's Nooh Alavi, I'm fifteen and I love programming! I taught myself how to code when I was ten, through books, YouTube videos, and of course, tons of Googling (thanks StackOverflow!).<br/><br/>
-          Now, as a hobby, I create video games, applications, and websites (like this one!) using <a href="/#software">a variety of languages and tools</a>. Check out some of my projects down below, and feel free to <a href="https://github.com/NoohAlavi" target="_blank">contribute</a> to them!
+          Hey there! My name's Nooh Alavi, I'm fifteen and I love programming! I taught myself how to code when I was ten, through books, YouTube videos, and of course, tons of Googling (thanks StackOverflow!).<br/><br/>
+          Now, as a hobby, I create video games, applications, and websites (like this one!) using <a href="/#software">a variety of languages and tools</a>. Check out some of my projects down below, and feel free to <a href="https://github.com/NoohAlavi" target="_blank">contribute to them</a>!
         </p>
       </div>
       <div className="container">
         <h3 className="page-subtitle" id="software">Software I Work With</h3>
+       <p className="content centered">
+         WORK IN PROGRESS
+       </p>
         {/* Things to add: Python, HTML, JS, CSS, REACTJS, GODOT */}
         <ul>
           
@@ -28,6 +31,9 @@ export default function Home() {
       <div className="container">
         <h3 className="page-subtitle" id="myProjects">My Projects</h3>     
       </div>
+      <p className="content centered">
+        Work in progress, but in the meantime, feel free to check out my <a href="https://github.com/NoohAlavi/">GitHub</a> and my <a href="https://noohalavi.itch.io/">Itch</a> pages!
+      </p>
       <div className="container">
         <h3 className="page-subtitle" id="contact">Contact Me</h3>
         <div>
